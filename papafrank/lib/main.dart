b5b9725720e369f1c87ce54fc83e3e244b8447e8
J <jacopobergamasco06@gmail.com>
2025-05-07 14:40:08 +0200

    pippo la merda

--- papafrank/lib/main.dart
+++ papafrank/lib/main.dart
@@ -1,119 +1,119 @@
-// Bergmasco Jacopo, 5AIA, A.S. 2024-2025
-// 23/04/2025, FloatingActionButton Multipli
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:papafrank/widget/sballo_button.dart';
 
 void main() {
-	runApp(const MyApp());
+    runApp(const MyApp());
 }
 
-const title = "Pippo LoSballo";
-
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
+    const MyApp({super.key});
 
-
-	@override
-	Widget build(BuildContext context) {
-		return const CupertinoApp(
-			title: title,
-			home: MyHomePage(title: title),
-		);
-	}
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(        
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+            ),
+            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
-	const MyHomePage({super.key, required this.title});
+    const MyHomePage({super.key, required this.title});
+    final String title;
 
-	final String title;
-
-	@override
-	State<MyHomePage> createState() => _MyHomePageState();
+    @override
+    State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-	int _answer = 0;
-    int _tentativi = 3;
-
-    final _quiz = [
+    var _questionIndex = 0;
+  	final questions = [
         {
-            'domanda': 'Come si chiama Loris Mandra?',
-            'sbagliate': ['Giuseppe', 'Loris', 'Noah'],
-            'giusta': 'Gabriel'
-        }
+            'questionText': 'What\'s your favorite color?',
+            'answers': ['Black', 'Red', 'Green', 'White'],
+            'correct': 'Green',
+        },
+        {
+            'questionText': 'What\'s your favorite animal?',
+            'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+            'correct': 'Elephant',
+        },
+        {
+            'questionText': 'Who\'s your favorite ITP instructor?',
+            'answers': ['John', 'Carl', 'Kevin', 'James'],
+            'correct': 'Kevin',
+        },
     ];
-    late int _quizIdx;
 
-    _MyHomePageState() {
-        _quizIdx = Random().nextInt(_quiz.length);
-    }
-
-    void _right() {
-        if (_tentativi > 0) {
-            setState(() {
-                _answer = 1;
-            });
-            _quizIdx = Random().nextInt(_quiz.length);
-        }
-    }
-
-    void _wrong() {
-        if (_tentativi > 0) {
-            setState(() {
-                _answer = -1;
-                _tentativi--;
-            });
-        }
-    }
-    void _rick() {
-        setState(() => _answer = 69);
-    }
-
-	@override
-	Widget build(BuildContext context) {
-		return CupertinoPageScaffold(
-            navigationBar: CupertinoNavigationBar(
-                middle: const Text(title),
-                trailing: CupertinoButton(
-                    onPressed: () => setState(() => _answer = 0),
-                    child: const Icon(Icons.refresh),
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: Text(widget.title),
+            ),
+            body: Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                width: double.infinity,
+                child: Center(          
+                    child: Column(            
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                            Text(
+                                questions[_questionIndex]['questionText'] as String,
+                                style: const TextStyle(
+                                    fontSize: 30
+                                ), 
+                            ),
+                            ...(questions[_questionIndex]['answers'] as List<String>)
+                            .map((answer) {
+                                return SballoButton(
+                                    text: answer,
+                                    action: () {},
+                                    background: Colors.lime,
+                                    primary: Colors.indigoAccent,
+                                );
+                            }),
+                            SballoButton(
+                                text: 'Next',
+                                action: () {},
+                                background: Colors.cyan,
+                                primary: Colors.white,
+                            ),
+                            if (true) const Text(
+                                'Risposta errata!!!',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 236, 12, 12),
+                                    fontSize: 50,
+                                ),
+                            ),
+                            if (true) const Text(
+                                'Risposta corretta!!!',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 12, 206, 12),
+                                    fontSize: 50,
+                                ),
+                            ),
+                            if (true) const Text(
+                                'Hai esaurito i tentativi!!!',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 23, 5, 211),
+                                    fontSize: 50,
+                                ),
+                            )
+                        ],
+                    ),
                 ),
             ),
-			child: Center (
-                child: Column (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                        Text(
-                            _quiz[_quizIdx]['domanda'] as String
-                        ),
-                        Column(
-                            children:
-                                [...(_quiz[_quizIdx]['sbagliate']), _quiz[_quizIdx]['giusta']]).map(
-                                    (elem) => SballoButton(text: elem, primary: Colors.amber, background: Colors.deepPurple, action: (elem == _quiz[_quizIdx]['giusta']) ? _right : _wrong)
-                                )
-                        ),
-                        Column(
-                            children: <Widget>[
-                                if (_answer < 0) Text('Ti restano $_tentativi tentativi!'),
-                                if (_answer == 1) const Icon(
-                                    Icons.check, 
-                                    size: 69.0,
-                                    color: Color.fromARGB(255, 0, 255, 0),
-                                ),
-                                if (_answer == -1) const Icon(
-                                    Icons.dangerous, 
-                                    size: 69.0,
-                                    color: Color.fromARGB(255, 255, 0, 0),
-                                ),
-                                if (_answer == 69) Image.asset("images/rick.gif")
-                            ],
-                        )
-                    ]
-			    ),
-			)
-		);
-	}
+            floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+            )
+        );
+    }
 }
